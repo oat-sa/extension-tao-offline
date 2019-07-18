13b247023aba26947a30e6e6d0256ffa5cae1e05
yuryfilipovich <yury.filipovich@1pt.com>
2019-07-18 19:01:01 +0300

    TAO-8694 - fix

--- controller/RestTestCenter.php
+++ controller/RestTestCenter.php
@@ -38,19 +38,177 @@ class RestTestCenter extends ParentRestTestCenterController
     ];
 
     /**
+     * @OA\Put(
+     *     path="/taoOffline/api/testCenter",
+     *     tags={"testCenter"},
+     *     summary="Update test center",
+     *     description="Update center organisation id or label",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="testCenter",
+     *                     type="string",
+     *                     description="Test center uri",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="label",
+     *                     type="string",
+     *                     description="Test center label",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="organisationId",
+     *                     type="string",
+     *                     description="Test center organisation id",
+     *                 ),
+     *                 required={"testCenter"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Test center URI",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="success",
+     *                     type="boolean",
+     *                     description="`false` on failure, `true` on success",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="uri",
+     *                     type="string",
+     *                     description="Updated test center URI",
+     *                 ),
+     *                 example={
+     *                     "success": true,
+     *                     "uri": "http://sample/first.rdf#i1536680377163171"
+     *                 }
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Invalid class uri",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 example={
+     *                     "success": false,
+     *                     "errorCode": 404,
+     *                     "errorMsg": "Test Center `http://sample/first.rdf#i1536680377163171` does not exist.",
+     *                     "version": "3.3.0-sprint106"
+     *                 }
+     *             )
+     *         ),
+     *     )
+     * )
+     */
+    public function put()
+    {
+       parent::put();
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/taoOffline/api/testCenter",
+     *     tags={"testCenter"},
+     *     summary="Create new test center",
+     *     description="Create new test center with organisation id",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="class-uri",
+     *                     type="string",
+     *                     description="Class uri to import item. If not specified root class will be used.",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="class-label",
+     *                     type="string",
+     *                     description="Label of class to import item. If not specified root class will be used.
+     *                     If label is not unique first match will be used.",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="label",
+     *                     type="string",
+     *                     description="Test center label",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="organisationId",
+     *                     type="string",
+     *                     description="Test center organisation id",
+     *                 ),
+     *                 required={"label", "organisationId"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Created test center URI",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="success",
+     *                     type="boolean",
+     *                     description="`false` on failure, `true` on success",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="uri",
+     *                     type="string",
+     *                     description="Created test center URI",
+     *                 ),
+     *                 example={
+     *                     "success": true,
+     *                     "uri": "http://sample/first.rdf#i1536680377163171"
+     *                 }
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid class uri",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 example={
+     *                     "success": false,
+     *                     "errorCode": 400,
+     *                     "errorMsg": "Class does not exist. Please use valid class-uri or class-label",
+     *                     "version": "3.3.0-sprint106"
+     *                 }
+     *             )
+     *         ),
+     *     )
+     * )
+     */
+    public function post()
+    {
+       parent::post();
+    }
+
+    /**
      * @param array $values
      * @return array
      * @throws common_exception_RestApi|common_exception_Error
      */
-    protected function prepareRequestData(array $values)
+    protected function validateRequestParameters(array $values)
     {
-        $propertiesValues = parent::prepareRequestData($values);
+        $propertiesValues = parent::validateRequestParameters($values);
 
         if (empty($values[self::PARAMETER_TEST_CENTER_ORGANISATION_ID])) {
             throw new common_exception_RestApi('Missed required parameter: organisationId');
         }
 
-        if(!$this->getValidatorsService()->validateOrganisationIdValue(
+        if (!$this->getValidatorsService()->validateOrganisationIdValue(
             $values[self::PARAMETER_TEST_CENTER_ORGANISATION_ID],
             array_key_exists('uri', $values) ? $values['uri'] : null
         )) {
