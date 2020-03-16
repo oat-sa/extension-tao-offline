906687722189ac2d3e56ebad3da714bb6ea555e6
Razhkou <yuryr@taotesting.com>
2020-03-16 14:51:53 +0300

    upgrade to phpunit8

--- test/unit/model/import/TestCenterMapperTest.php
+++ test/unit/model/import/TestCenterMapperTest.php
@@ -38,7 +38,7 @@ class TestCenterMapperTest extends TestCase
      */
     private $mapper;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
