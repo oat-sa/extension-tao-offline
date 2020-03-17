2e90f3826cb76f009f94ad4b65698f5fcc03576c
Razhkou <yuryr@taotesting.com>
2020-03-17 14:32:43 +0300

    dependency generis 12.15.0. Removed some assertTrue from risky tests.

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return array(
     'version' => '3.1.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'generis'       => '>=12.5.0',
+        'generis'       => '>=12.15.0',
         'taoTestCenter' => '>=8.2.0',
         'taoSync'       => '>=4.2.0',
         'taoPublishing' => '>=2.1.1',
